180058add4a989a20df569056d3a1237856e9f80
carlos-toscano <lic.carlostoscano@gmail.com>
2022-12-01 22:10:50 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("44ad6e1f-bc81-42a9-a9cc-99dc6908000a");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
